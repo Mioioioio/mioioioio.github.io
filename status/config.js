de504f29df934bacb1d8a8dac2b3bddb6dae0c73
Mioioioio <chancesitu@gmail.com>
2022-08-16 20:43:58 +0800

    Site updated: 2022-08-16 20:43:56

---
+++ status/config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '御庭监控室(￣▽￣)"',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1829711-ffa46a8da06fa15648341f3e',
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://int.nakiri.cc/'
+    },
+  ],
+};
